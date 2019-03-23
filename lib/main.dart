dab726a25030fbd59a0cd25612140bcf3232a5dd
Mateus <mateusferreira17@yahoo.com.br>
2019-03-23 11:02:31 -0300

    criação da tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -7,54 +7,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Alconomia',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginHomePage(title: 'AlcoNomia'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class LoginHomePage extends StatefulWidget {
+  LoginHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LoginHomePageState createState() => _LoginHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginHomePageState extends State<LoginHomePage> {
+  final scaffoldKey = new GlobalKey<ScaffoldState>();
+  final formkey = new GlobalKey<FormState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String email;
+  String password;
+
+  void _submit() {
+    final form = formkey.currentState;
+
+    if (form.validate()) {
+      form.save();
+
+      performLogin();
+    }
+  }
+
+  void performLogin() {
+    final snackbar = new SnackBar(
+      content: new Text("Email : $email, password : $password"),
+    );
+    scaffoldKey.currentState.showSnackBar(snackbar);
   }
 
   @override
@@ -74,38 +66,36 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
+        key: formkey,
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            new TextFormField(
+              decoration: new InputDecoration(labelText: "E-mail"),
+              validator: (val) => !val.contains('@') ? 'E-mail invalido' : null,
+              onSaved: (val) => email = val,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            new TextFormField(
+              decoration: new InputDecoration(labelText: "Password"),
+              validator: (val) =>
+                  val.isEmpty ? 'A senha não pode ser vazia' : null,
+              onSaved: (val) => password = val,
             ),
+            new Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+            ),
+            new RaisedButton(
+              child: new Text(
+                "login",
+                style: new TextStyle(color: Colors.white),
+              ),
+              color: Colors.blue,
+              onPressed: _submit,
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
